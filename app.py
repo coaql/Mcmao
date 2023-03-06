085514e0ffc599e24b9991f6fd39398c15f5c52d
coaql <124441614+coaql@users.noreply.github.com>
2023-03-06 08:49:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
+REGISTER = []
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def register():
+    return render_template("register.html")
+
+@app.route("/success")
+def sucess():
+    name = request.args.get("username")
+    if name in REGISTER :
+        return f"Username already registered!!"
+    REGISTER.append(name)
+    return f"sucessfully registered."
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/home")
+def home():
+    #check if user is in register list
+    name = request.args.get("username")
+    if name not in REGISTER :
+        return render_template("unsuccess.html")
+
+    return render_template("home.html",name = name)
+
